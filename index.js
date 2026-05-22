23b30a04d8a827fdec7cf91547bb0b0d233f9959
kazisamin0173-byte <kazisamin0173@gmail.com>
2026-05-22 22:26:02 +0600

    some add

--- index.js
+++ index.js
@@ -837,6 +837,16 @@ async function run() {
   }
 }
 
+// app.use((req, res, next) => {
+
+//   console.log(
+//     req.method,
+//     req.url
+//   );
+
+//   next();
+// });
+
 await run();
 
 // app.listen(port, () => {
